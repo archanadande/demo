b95c7a9ca2960f9d5cc5401cc899379f913eb6e5
Dell 2 <Dell 2@DESKTOP-RQIIACK>
2022-12-13 16:32:08 +0530

    changes

---
+++ src/com/archu/first.java
@@ -0,0 +1,23 @@
+package com.archu;
+
+public class first {
+public static void main(String[] args) {
+	int a=10;
+	int b=20;
+	int c=30;
+	int d=40;
+	int sum;
+	int sub;
+	int div;
+	int mul;
+	sum = a+b;
+	sub = c-b;
+	div = d/b;
+	mul = a*b;
+	System.out.println("addition is :-"+sum);
+	System.out.println("subtraction is :-"+sub);
+	System.out.println("division is :-"+div);
+	System.out.println("multiplication is :-"+mul);
+	
+}
+}
